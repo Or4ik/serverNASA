13659911af7489977f9e5bc1cb7db892304248e0
Or4ik <orestsp1@gmail.com>
2016-03-25 21:49:44 +0200

    create url object

--- static/js/script.js
+++ static/js/script.js
@@ -2,7 +2,12 @@ window.onload = function() {
 	var changeContentButton = document.getElementById('send-request');
 	var showMarsImagesButton = document.getElementById('show-mars-images');
 	var showMarsImages2Button = document.getElementById('show-mars-images-2');
+	var showMoreMarsImagesButton = document.getElementById('show--more-mars-images');
+
 	var marsImages = document.getElementById('mars-images');
+	
+	var urlApi = new URL("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=CM1T7kQolcZFhhewvRLrI2QRikGKizQ0AFp7PLGo");
+
 
 	changeContentButton.addEventListener('click', function(event) {
 		xhr.get('data/data.json', function(response) {
@@ -11,7 +16,7 @@ window.onload = function() {
 	});
 
 	showMarsImagesButton.addEventListener('click', function(event) {
-		xhr.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=CM1T7kQolcZFhhewvRLrI2QRikGKizQ0AFp7PLGo', function(response) {
+		xhr.get(urlApi, function(response) {
 			var responseObj = JSON.parse(response);
 			var photoUrls = createPhotoUrlsFromResponse(responseObj);
 			showPhotos(photoUrls);
